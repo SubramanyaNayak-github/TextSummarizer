81d1bb0c9f696244c1cb06ada70393e5f720272e
SubramanyaNayak <subramanyanayak3@gmail.com>
2023-12-14 13:42:20 +0530

    FastAPI APP

---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from src.textsummarizer.pipeline.prediction  import PredictionPipeline
+
+
+text:str = 'What is Text Summarization?'
+
+app = FastAPI()
+
+
+@app.get('/',tags=['authentication'])
+async def index():
+    return RedirectResponse(url='/docs')
+
+
+@app.get('/train')
+async def training():
+    try:
+        os.system('python main.py')
+        return Response('Training Successful!')
+    
+    except Exception as e:
+        return Response('Error Occurred! {}'.format(e))
+
+
+
+@app.get('/predict')
+async def prediction(text):
+
+    try:
+        obj = PredictionPipeline()
+        res = obj.predict(text)
+        return res
+
+    except Exception as e:
+        raise e
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = '0.0.0.0',port =5500)
